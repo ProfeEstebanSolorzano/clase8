8e9da721ac90e594a5cd75351f714443411bf446
EstebanSolorzano <chewva_k@hotmail.com>
2021-10-11 11:04:01 -0600

    clase lista

---
+++ fetch.js
@@ -0,0 +1,5 @@
+const boton = document.querySelector('#boton');
+
+boton.onclick = (e) => {
+    console.log('test')
+}
---
+++ myscript.js
@@ -0,0 +1,6 @@
+const boton = document.querySelector('#boton');
+
+boton.onclick = (e) => {
+    console.log('test');
+
+}
